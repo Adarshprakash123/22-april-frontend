e397cca5ebcee6a64eaf2eb2c38bd6e35146072b
Adarsh Prakash <adarshsingh458868@gmail.com>
2026-04-22 17:46:06 +0530

    chnages added

--- app/lib/api.js
+++ app/lib/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API=axios.create({
-    baseURL:"http://localhost:3000/api"
+    baseURL:"https://two2-april.onrender.com/api"
 })
 
 API.interceptors.request.use((req)=>{
